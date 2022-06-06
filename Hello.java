3e33e9633418d4e1796b2be38fc69338e4918a85
Shaffi1999 <104353585+Shaffi1999@users.noreply.github.com>
2022-06-06 15:49:41 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -2,6 +2,8 @@ class Hello
 {
     public static void main(String args[])
     {
-          System.out.println("Hello World");
+          System.out.println("Hello World1");
+          System.out.println("Hello World2");
+        
     }
 }
